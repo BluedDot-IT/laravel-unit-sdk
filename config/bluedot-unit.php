034b715d9c887fad6fdcbc67b2394a7985e8a8e6
Taner Tombaş <tombastaner@gmail.com>
2023-04-30 18:30:27 +0300

    update config

--- config/bluedot-unit.php
+++ config/bluedot-unit.php
@@ -4,11 +4,12 @@ use Illuminate\Http\Request;
 
 return [
     "api" => [
-        "base_uri" => "https://api.s.unit.sh/"
+        "base_uri" => env("BLUEDOT_UNIT_BASE_URL", "https://api.s.unit.sh/"),
+        'auth-token' => env("BLUEDOT_UNIT_API_TOKEN"),
+        "content-type" => "application/vnd.api+json"
     ],
     "Allowed-methods" => [
         Request::METHOD_POST, Request::METHOD_GET
-    ],
-    "content-type" => "application/vnd.api+json"
+    ]
 
 ];
